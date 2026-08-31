5117ac6f49b5a43402b68b0940e3a59323fc58e3
justthatrandomcoder <me@juliusgrimm.dev>
2026-08-31 10:46:24 +0200

    chore(repo): add the Xcode project scaffold
    Xcode 26 project, synchronized folders, iOS/iPadOS 26.2, no dependencies.

---
+++ Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by  Julius Grimm on 31.08.26.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
